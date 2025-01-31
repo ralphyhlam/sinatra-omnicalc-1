7d4f248c90228cc6ee295572339d123f23b0a851
ralphyhlam <ralphyhlam@uchicago.edu>
2025-01-31 23:24:47 +0000

    Got up to squaring and rooting

--- app.rb
+++ app.rb
@@ -2,8 +2,27 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)  
+end
+
+get("/square/new") do 
+  erb(:square)
+end
+
+get("/square/results") do
+  @input = params.fetch("number").to_i
+  @squared = @input ** (2)
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:squareroot)
+end
+
+get("/squareroot/results") do
+  @input = params.fetch("user_number").to_i
+  @rooted = @input ** (1/2)
+
+  erb(:squareroot_results)
 end
